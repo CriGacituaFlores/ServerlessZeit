c55523f55aee3a56e22b7c4433e2803b056d7f8f
Cristian <cgacitua@acid.cl>
2020-02-08 12:40:10 -0300

    configure routes

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const plates = require('./routes/plates');
+const orders = require('./routes/orders');
 const app = express()
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+//use router in url
+app.use('/api/plates', plates)
+app.use('/api/orders', orders)
 
-app.get('*', (req, res) => {
-  res.send('Hola Mundo')
-})
 
 module.exports = app;
